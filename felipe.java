4c2ed2eae52c82277d41821ebe5a0195935cfe8f
lfv.vercosa <lfv.vercosa@gmail.com>
2013-02-13 18:05:44 -0500

    correcting file

--- felipe.java
+++ felipe.java
@@ -2,6 +2,6 @@ String rubem = "";
 if(ruben.equals("gay")){
 	System.out.println("I've already known");
 }else{
-	System.out.println("Vai po caralho");
+	System.out.println("I'm sure it's a lie");
 }
 
