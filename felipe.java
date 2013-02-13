549f3badc4ee0a5ce4779367a1c9981d11b55c77
Rúben <rubengomesff@gmail.com>
2013-02-13 18:38:21 -0500

    toma no cu

--- felipe.java
+++ felipe.java
@@ -2,9 +2,6 @@ String rubem = "";
 if(ruben.equals("gay")){
 	System.out.println("I've already known");
 }else{
-	System.out.println("Vai po caralho meu irmao");
-	System.out.println("cona");
-
-	System.out.println("I'm sure it's a lie");
+	
 }
 
